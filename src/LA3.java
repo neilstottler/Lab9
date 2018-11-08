ae1cb062b9e937f35bcb596be6714e0f508c9c81
neilstottler <2000nesman@gmail.com>
2018-11-08 09:15:54 -0500

    reorganized code

--- src/LA3.java
+++ src/LA3.java
@@ -27,47 +27,43 @@ public class LA3 {
 		
 			//min of all numbers
 			case "min": operation = ""; 
-					double min;
-					min = Math.min(num1, Math.min(num3, num2));
-					System.out.println(" Min(" + num1 + ", " +  num2 + ", " + num3 + ") = "+ df.format(min));
+				double min;
+				min = Math.min(num1, Math.min(num3, num2));
+				System.out.println(" Min(" + num1 + ", " +  num2 + ", " + num3 + ") = "+ df.format(min));
 			break;
 			
 			//absolute value
 			case "l1" : operation = ""; 
-			double absnum1;
-			double absnum2;
-			double absnum3;
+				double absnum1;
+				double absnum2;
+				double absnum3;
 			
 			//get absolute
-			absnum1 = Math.abs(num1);
-			absnum2 = Math.abs(num2);
-			absnum3 = Math.abs(num3);
+				absnum1 = Math.abs(num1);
+				absnum2 = Math.abs(num2);
+				absnum3 = Math.abs(num3);
 			
-			System.out.println(" l1(" + num1 + ", " +  num2 + ", " + num3 + ") = "+ df.format(absnum1 + absnum2 + absnum3));
+				System.out.println(" l1(" + num1 + ", " +  num2 + ", " + num3 + ") = "+ df.format(absnum1 + absnum2 + absnum3));
 			
-			break;
+				break;
 			
 			//sqrt of sum
 			case "l2" : operation = ""; 
 			
-			
-			double sqrtnum1;
-			double sqrtnum2;
-			double sqrtnum3;
+				double sqrtnum1;
+				double sqrtnum2;
+				double sqrtnum3;
 			
 			//get sqrt
-			double sqnum1 = num1 * num1;
-			double sqnum2 = num2 * num2;
-			double sqnum3 = num3 * num3;
+				double sqnum1 = num1 * num1;
+				double sqnum2 = num2 * num2;
+				double sqnum3 = num3 * num3;
 			
-			sqrtnum1 = Math.sqrt(sqnum1);
-			sqrtnum2 = Math.sqrt(sqnum2);
-			sqrtnum3 = Math.sqrt(sqnum3);
-			
-			double sqrtsum = sqrtnum1 + sqrtnum2 + sqrtnum3;
-			System.out.println(sqrtnum1);
-			System.out.println(sqrtnum2);
-			System.out.println(sqrtnum2);
+				sqrtnum1 = Math.sqrt(sqnum1);
+				sqrtnum2 = Math.sqrt(sqnum2);
+				sqrtnum3 = Math.sqrt(sqnum3);
+				
+				double sqrtsum = sqrtnum1 + sqrtnum2 + sqrtnum3;
 			
 			//output
 			System.out.println(" l2(" + num1 + ", " +  num2 + ", " + num3 + ") = "+ df.format(sqrtsum));
